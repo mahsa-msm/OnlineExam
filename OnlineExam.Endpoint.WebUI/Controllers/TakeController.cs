882f16387e1b8f01615ece5b8878d47ddf63757f
mahsa-msm <mahsaa.mousavi@yahoo.com>
2020-09-06 06:15:08 -0700

    ugly dynamic text box for add question

--- OnlineExam.Endpoint.WebUI/Controllers/TakeController.cs
+++ OnlineExam.Endpoint.WebUI/Controllers/TakeController.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Nancy.Json;
 using OnlineExam.Domain.Contracts.Answers;
+using OnlineExam.Domain.Contracts.Choices;
 using OnlineExam.Domain.Contracts.Courses;
 using OnlineExam.Domain.Contracts.ExamQuestions;
 using OnlineExam.Domain.Contracts.Exams;
+using OnlineExam.Domain.Contracts.QuestionChoices;
 using OnlineExam.Domain.Contracts.Questions;
 using OnlineExam.Domain.Core.Choices;
 using OnlineExam.Domain.Core.ExamQuestions;
+using OnlineExam.Domain.Core.QuestionChoices;
+using OnlineExam.Domain.Core.Questions;
 using OnlineExam.Endpoint.WebUI.Models.Exams;
 
 namespace OnlineExam.Endpoint.WebUI.Controllers
@@ -20,16 +25,22 @@ namespace OnlineExam.Endpoint.WebUI.Controllers
         private readonly IQuestionRepository questionRepository;
         private readonly IExamRepository examRepository;
         private readonly IAnswerRepository answerRepository;
+        private readonly IChoiceRepository choiceRepository;
+        private readonly IQuestionChoiceRepository questionChoiceRepository;
 
         public TakeController(IExamQuestionRepository examQuestionRepository,
             IQuestionRepository questionRepository,
             IExamRepository examRepository,
-             IAnswerRepository answerRepository)
+             IAnswerRepository answerRepository,
+             IChoiceRepository choiceRepository,
+             IQuestionChoiceRepository questionChoiceRepository)
         {
             this.examQuestionRepository = examQuestionRepository;
             this.questionRepository = questionRepository;
             this.examRepository = examRepository;
             this.answerRepository = answerRepository;
+            this.choiceRepository = choiceRepository;
+            this.questionChoiceRepository = questionChoiceRepository;
         }
 
 
@@ -45,7 +56,7 @@ namespace OnlineExam.Endpoint.WebUI.Controllers
         }
 
 
-        public JsonResult TakeExam(  int examID = 1)
+        public JsonResult TakeExam(int examID = 1)
         {
             GiveExamViewModel takeExam = new GiveExamViewModel();
             ViewBag.examId = examID;
@@ -54,17 +65,18 @@ namespace OnlineExam.Endpoint.WebUI.Controllers
             takeExam.Questions = examQuestions.Select(c => c.Question).ToList();
 
             var t = takeExam.Questions.Select(x => x.QuestionChoices.Select(x => x.Choice.Text));
-    //        return new JsonResult(takeExam);
+            //        return new JsonResult(takeExam);
 
-            return Json(new {
-            
+            return Json(new
+            {
+
                 examName = takeExam.ExamId,
                 examResponse = getExamAnswers(takeExam.ExamId),
 
             });
         }
 
-        private string getExamAnswers(int examId) 
+        private string getExamAnswers(int examId)
         {
             var examQuestions = examQuestionRepository.GetExamQuestions(examId);
 
@@ -84,6 +96,35 @@ namespace OnlineExam.Endpoint.WebUI.Controllers
 
             return answers;
         }
-  
+        public ActionResult Take2()
+        {
+            return View();
+        }
+
+        // POST: Home
+        [HttpPost]
+        public ActionResult Take2(string[] DynamicTextBox , QuestionViewModel questionViewModel)
+        {
+            JavaScriptSerializer serializer = new JavaScriptSerializer();
+
+            Question question = new Question { Text = questionViewModel.Text };
+            questionRepository.Add(question);
+
+            //Loop through the dynamic TextBox values.
+            foreach (string textboxValue in DynamicTextBox)
+            {
+                Choice choice = new Choice { Text = textboxValue };
+                choiceRepository.Add(choice);
+                QuestionChoice questionChoice = new QuestionChoice
+                {
+                    Choice = choice,
+                    Question = question
+                };
+                questionChoiceRepository.Add(questionChoice);
+
+            }
+            return View();
+        }
+   
     }
 }
