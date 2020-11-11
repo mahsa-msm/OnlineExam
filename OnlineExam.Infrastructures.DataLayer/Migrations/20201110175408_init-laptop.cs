e95548bc8bfc78cb46cfa59e808f21692c255e88
TurboRayan <TurboRayan@DESKTOP-K84TPHK>
2020-11-11 02:07:56 -0800

    course datatable

---
+++ OnlineExam.Infrastructures.DataLayer/Migrations/20201110175408_init-laptop.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OnlineExam.Infrastructures.DataLayer.Migrations
+{
+    public partial class initlaptop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
